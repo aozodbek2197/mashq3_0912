32b2d44f011a072f4bbe5930995256d7b39aabee
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-09 16:16:16 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# # 14-masala
+# roy = ["car", "bike", "bus"]
+# teskari = []
+# for i in roy[::-1]:
+#     teskari.append(i)
+# print(teskari)
+# # 15-masala
+# roy = [1, 2, 3, 4]
+# barobar = [i*2 for i in roy ]
+# print(barobar)
+# # 16-masala
+# roy = [65, 85, 45, 95, 70]
+# ball = [i for i in roy if i > 70]
+# print(ball)
+# # 17-masala
+# matn = ["hello", "123", "world", "456"]
+# filtrlash = [i for i in matn if i.isalpha()]
+# print(filtrlash)
+# # 18-masala
+# qoldq_filter = []
+# for i in range(1, 21):
+#     if i % 3 == 1:
+#         qoldq_filter.append(i)
+# print(qoldq_filter)
